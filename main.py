6914b837bd8a97979af5852bf6e34f5ced076c21
saivenkatesh <saivenkatesh619@gmail.com>
2024-02-21 00:02:09 +0530

    Add Required number of pages for notes for each topic

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ for index,item in df.iterrows():
     pdf.set_text_color(100,100,100) #gray color
     pdf.cell(w=0,h=10,txt=item["Topic"],ln=1,align='L')
     pdf.line(10,21,200,21)
+    if int(item["Pages"]) !=1: #adding pages
+        for i in range(int(item["Pages"])-1):
+            pdf.add_page()
 
 
 pdf.output('output.pdf')
